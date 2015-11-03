61164479aaa46cf18285a7f7775d087093bb032f
Janwillem Swalens <jswalens@vub.ac.be>
2015-11-03 12:50:14 +0100

    Copy benchmark results to my home directory.

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo "Benchmark working"
+echo "Benchmarking..."
+
+REV=`git rev-parse HEAD | cut -c1-8`
+TARGET_PATH=/home/jswalens/ecoop-2016-results/$REV/
+mkdir $TARGET_PATH
+#cp results $TARGET_PATH
+
+echo "Benchmark done"
