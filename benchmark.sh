2bcf5cd94daea6b710b01321a2365446de2bab2f
Janwillem Swalens <jswalens@vub.ac.be>
2015-11-05 12:54:36 +0100

    Use input with n = 32.

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ date=`date "+%Y-%m-%dT%H:%M:%S%Z"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-tests="random-x64-y64-z3-n4.txt random-x64-y64-z3-n48.txt"
+tests="random-x64-y64-z3-n32"
 
 lein=$pwd/lein
 
@@ -27,12 +27,12 @@ do
       echo "i=$i; test=$test; t=$t"
       version="llcc-2.3"
       cd $pwd/$version
-  		$lein run -i $pwd/$test -t $t > $result_path/$test-$version-t$t-i$i.txt
+  		$lein run -i $pwd/$test.txt -t $t > $result_path/$test-$version-t$t-i$i.txt
       version="pbcc-3"
       cd $pwd/$version
       for a in 1 2 4 8 16
       do
-        $lein run -i $pwd/$test -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
+        $lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
       done
   	done
   done
