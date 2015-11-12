950f519ceff79662b94eaee064f583db56adba0f
Janwillem Swalens <jswalens@vub.ac.be>
2015-11-12 17:27:23 +0100

    Only test one test, but with a range of JVM options.

--- benchmark.sh
+++ benchmark.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-pwd=`pwd`
+pwd="`pwd`"
 rev=`git rev-parse HEAD | cut -c1-8`
-date=`date "+%Y-%m-%dT%H:%M:%S%Z"`
+date=`date "+%Y%m%dT%H%M"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-tests="random-x32-y32-z3-n4 random-x32-y32-z3-n8 random-x32-y32-z3-n10 random-x32-y32-z3-n16 random-x32-y32-z3-n32 random-x32-y32-z3-n48 random-x32-y32-z3-n64 random-x48-y48-z3-n4 random-x48-y48-z3-n8 random-x48-y48-z3-n16 random-x48-y48-z3-n32 random-x64-y64-z3-n4 random-x64-y64-z3-n8 random-x64-y64-z3-n10 random-x64-y64-z3-n16 random-x64-y64-z3-n32"
+tests="random-x64-y64-z3-n32"
 
 lein=$pwd/lein
 
@@ -14,25 +14,42 @@ echo "Installing lein..."
 $lein version
 echo "lein installed"
 
+echo "Making uberjars"
+cd $pwd/llcc-2.3
+$lein uberjar
+cd $pwd/pbcc-3
+$lein uberjar
+echo "Uberjars made"
+
 echo "Benchmarking..."
 
 mkdir -p $result_path
 
-for i in 1 2 3
+for i in 1 2 3 4 5 # five now
 do
   for test in $tests
   do
-    for t in 1 2 4 8 # 16 - don't do t=16 anymore for now
+    for opts in "" "-Xmx8192m"
     do
-      echo "i=$i; test=$test; t=$t"
-      version="llcc-2.3"
-      cd $pwd/$version
-      $lein run -i $pwd/$test.txt -t $t > $result_path/$test-$version-t$t-i$i.txt
-      version="pbcc-3"
-      cd $pwd/$version
-      for a in 1 2 4 8 16
+      for gc_opts in "" "-XX:+UseSerialGC" "-XX:+UseParallelGC -XX:-UseParallelOldGC" "-XX:+UseParallelGC -XX:+UseParallelOldGC" "-XX:+UseConcMarkSweepGC -XX:+UseParNewGC" "-XX:+UseG1GC"
       do
-        $lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
+        for t in 1 2 4 8 # 16
+        do
+          echo "i=$i; test=$test; t=$t; opts=$opts; gc_opts=$gc_opts"
+          version="llcc-2.3"
+          cd $pwd/$version
+          echo "version=$version"
+          java $opts $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t > $result_path/$test-$opts-$gc_opts-$version-t$t-i$i.txt
+          #$lein run -i $pwd/$test.txt -t $t > $result_path/$test-$version-t$t-i$i.txt
+          version="pbcc-3"
+          cd $pwd/$version
+          for a in 1 2 4 8 # 16
+          do
+            echo "version=$version; a=$a"
+            java $opts $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$opts-$gc_opts-$version-t$t-a$a-i$i.txt
+            #$lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
+          done
+        done
       done
     done
   done
