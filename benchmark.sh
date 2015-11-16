b17f7c26037f65139ee0e672f5379c59ba6da2bd
Janwillem Swalens <jswalens@vub.ac.be>
2015-11-16 14:38:26 +0100

    Try lein vs java uberjar, with and without JVM options.

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ date=`date "+%Y%m%dT%H%M"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-tests="random-x48-y48-z3-n16 random-x64-y64-z3-n16"
+tests="random-x64-y64-z3-n32"
 
 lein=$pwd/lein
 
@@ -37,15 +37,15 @@ do
         version="llcc-2.3"
         cd $pwd/$version
         echo "version=$version"
-        java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t > $result_path/$test-$opts-$gc_opts-$version-t$t-i$i.txt
-        #$lein run -i $pwd/$test.txt -t $t > $result_path/$test-$version-t$t-i$i.txt
+        java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t > $result_path/$test-java-$gc_opts-$version-t$t-i$i.txt
+        JVM_OPTS="$gc_opts" $lein run -i $pwd/$test.txt -t $t > $result_path/$test-lein-$gc_opts-$version-t$t-i$i.txt
         version="pbcc-3"
         cd $pwd/$version
         for a in 1 2 4 8 16
         do
           echo "version=$version; a=$a"
-          java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$opts-$gc_opts-$version-t$t-a$a-i$i.txt
-          #$lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
+          java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t -a $a > $result_path/$test-java-$gc_opts-$version-t$t-a$a-i$i.txt
+          JVM_OPTS="$gc_opts" $lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-lein-$gc_opts-$version-t$t-a$a-i$i.txt
         done
       done
     done
@@ -54,7 +54,7 @@ done
 
 mkdir $target_path
 chmod 0777 $target_path
-cp -R $result_path/ $target_path
+cp -R $result_path/* $target_path
 chmod -R 0777 $target_path
 
 echo "Benchmark done"
