62f5a058fc9ebb756eecc3dfd8ac7a528479e576
Janwillem Swalens <jswalens@vub.ac.be>
2015-11-16 10:37:12 +0100

    Use other test inputs but vary fewer GC options.

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ date=`date "+%Y%m%dT%H%M"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-tests="random-x64-y64-z3-n32"
+tests="random-x48-y48-z3-n16 random-x64-y64-z3-n16"
 
 lein=$pwd/lein
 
@@ -29,26 +29,23 @@ for i in 1 2 3 4 5 # five now
 do
   for test in $tests
   do
-    for opts in "" "-Xmx8192m"
+    for gc_opts in "" "-XX:+UseG1GC"
     do
-      for gc_opts in "" "-XX:+UseSerialGC" "-XX:+UseParallelGC -XX:-UseParallelOldGC" "-XX:+UseParallelGC -XX:+UseParallelOldGC" "-XX:+UseConcMarkSweepGC -XX:+UseParNewGC" "-XX:+UseG1GC"
+      for t in 1 2 4 8 16
       do
-        for t in 1 2 4 8 # 16
+        echo "i=$i; test=$test; t=$t; gc_opts=$gc_opts"
+        version="llcc-2.3"
+        cd $pwd/$version
+        echo "version=$version"
+        java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t > $result_path/$test-$opts-$gc_opts-$version-t$t-i$i.txt
+        #$lein run -i $pwd/$test.txt -t $t > $result_path/$test-$version-t$t-i$i.txt
+        version="pbcc-3"
+        cd $pwd/$version
+        for a in 1 2 4 8 16
         do
-          echo "i=$i; test=$test; t=$t; opts=$opts; gc_opts=$gc_opts"
-          version="llcc-2.3"
-          cd $pwd/$version
-          echo "version=$version"
-          java $opts $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t > $result_path/$test-$opts-$gc_opts-$version-t$t-i$i.txt
-          #$lein run -i $pwd/$test.txt -t $t > $result_path/$test-$version-t$t-i$i.txt
-          version="pbcc-3"
-          cd $pwd/$version
-          for a in 1 2 4 8 # 16
-          do
-            echo "version=$version; a=$a"
-            java $opts $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$opts-$gc_opts-$version-t$t-a$a-i$i.txt
-            #$lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
-          done
+          echo "version=$version; a=$a"
+          java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$opts-$gc_opts-$version-t$t-a$a-i$i.txt
+          #$lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$version-t$t-a$a-i$i.txt
         done
       done
     done
