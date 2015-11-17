d1925c27ee539cbfc57ab1c48074114adb28f1df
Janwillem Swalens <jswalens@vub.ac.be>
2015-11-17 15:41:58 +0100

    Do test with n = 4; only use lein
    Note: /bin/sh to /bin/bash, needed for {1..5} syntax.

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 pwd="`pwd`"
 rev=`git rev-parse HEAD | cut -c1-8`
@@ -6,7 +6,7 @@ date=`date "+%Y%m%dT%H%M"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-tests="random-x64-y64-z3-n32"
+tests="random-x64-y64-z3-n4 random-x64-y64-z3-n32"
 
 lein=$pwd/lein
 
@@ -25,7 +25,7 @@ echo "Benchmarking..."
 
 mkdir -p $result_path
 
-for i in 1 2 3 4 5 # five now
+for i in {1..5}
 do
   for test in $tests
   do
@@ -37,20 +37,20 @@ do
         version="llcc-2.3"
         cd $pwd/$version
         echo "version=$version"
-        java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t > $result_path/$test-java-$gc_opts-$version-t$t-i$i.txt
-        JVM_OPTS="$gc_opts" $lein run -i $pwd/$test.txt -t $t > $result_path/$test-lein-$gc_opts-$version-t$t-i$i.txt
+        JVM_OPTS="$gc_opts" $lein run -i $pwd/$test.txt -t $t > $result_path/$test-$gc_opts-$version-t$t-i$i.txt
         version="pbcc-3"
         cd $pwd/$version
         for a in 1 2 4 8 16
         do
           echo "version=$version; a=$a"
-          java $gc_opts -cp resources/clojure-1.6.0-transactional-futures-2.3.jar:target/uberjar/labyrinth-0.1.0-SNAPSHOT-standalone.jar labyrinth.main -i $pwd/$test.txt -t $t -a $a > $result_path/$test-java-$gc_opts-$version-t$t-a$a-i$i.txt
-          JVM_OPTS="$gc_opts" $lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-lein-$gc_opts-$version-t$t-a$a-i$i.txt
+          JVM_OPTS="$gc_opts" $lein run -i $pwd/$test.txt -t $t -a $a > $result_path/$test-$gc_opts-$version-t$t-a$a-i$i.txt
         done
       done
     done
   done
 done
+
+echo "Copying results..."
 
 mkdir $target_path
 chmod 0777 $target_path
