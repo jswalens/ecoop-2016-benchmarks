f035035057b0a2f0e0b06cec302b1e25e09895cc
JW <JW00000@gmail.com>
2015-11-28 20:37:24 +0100

    Try different GC options.

--- benchmark.sh
+++ benchmark.sh
@@ -6,8 +6,8 @@ date=`date "+%Y%m%dT%H%M"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-#pars="-n 4 -r 512"
-gc_opts="-XX:+UseG1GC"
+pars="-n 4 -r 512"
+#gc_opts="-XX:+UseG1GC"
 
 lein=$pwd/lein
 
@@ -23,20 +23,20 @@ cd $pwd/bayes-8
 
 for i in {1..3}
 do
-  for pars in "-n 5 -r 256" "-n 5 -r 512" "-n 5 -r 750" "-n 6 -r 512"
+  for gc_opts in "" "-XX:+UseG1GC" "-XX:+UseConcMarkSweepGC -XX:-UseParNewGC" "-XX:+UseConcMarkSweepGC -XX:+UseParNewGC" "-XX:+UseParallelGC"
   do
-    pars_filename="${pars// /_}"
-    for v in 16 32 50 64 75 # 128
+    gc_filename="${gc_opts// /_}"
+    for v in 8 16 32 64 # 128
     do
-      for t in 1 2 4 8 16 32 # 64
+      for t in 1 2 4 8 16 32 64
       do
         echo "i=$i; v=$v; t=$t; parameters=$pars; gc_opts=$gc_opts"
         variation=""
         echo "variation=$variation"
-        JVM_OPTS="$gc_opts" $lein run $pars -v $v -t $t               > $result_path/$variation-$pars_filename-v$v-t$t-i$i.txt
+        JVM_OPTS="$gc_opts" $lein run $pars -v $v -t $t               > $result_path/$variation-$gc_filename-v$v-t$t-i$i.txt
         variation="alternatives-parallel"
         echo "variation=$variation"
-        JVM_OPTS="$gc_opts" $lein run $pars -v $v -t $t -x $variation > $result_path/$variation-$pars_filename-v$v-t$t-i$i.txt
+        JVM_OPTS="$gc_opts" $lein run $pars -v $v -t $t -x $variation > $result_path/$variation-$gc_filename-v$v-t$t-i$i.txt
       done
     done
   done
