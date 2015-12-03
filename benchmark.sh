904f5b9f78eac8f64de7db932c247ea6cea882d4
Janwillem Swalens <jswalens@vub.ac.be>
2015-12-03 13:02:39 +0100

    Run again three combinations of parameters.

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ date=`date "+%Y%m%dT%H%M"`
 result_path="$pwd/results/$date-$rev"
 target_path="/home/jswalens/ecoop-2016-results/$date-$rev"
 
-pars="-n 4 -r 64"
+#pars="-n 5 -r 256"
 gc_opts="-XX:+UseG1GC"
 
 lein=$pwd/lein
@@ -21,19 +21,20 @@ mkdir -p $result_path
 
 cd $pwd/bayes-8
 
-for i in {1..10}
+for i in {1..5}
 do
-  for v in 32
+  for pars in "-n 5 -r 256 -v 32" "-n 5 -r 256 -v 64" "-n 5 -r 512 -v 48"
   do
+    pars_filename="${pars// /_}"
     for t in 1 2 4 8 16 32 64
     do
       echo "i=$i; v=$v; t=$t; parameters=$pars; gc_opts=$gc_opts"
-      variation=""
+      variation="original"
       echo "variation=$variation"
-      JVM_OPTS="$gc_opts" $lein run $pars -v $v -t $t               > $result_path/$variation-v$v-t$t-i$i.txt
+      JVM_OPTS="$gc_opts" $lein run $pars -t $t               > $result_path/$variation-$pars_filename-t$t-i$i.txt
       variation="alternatives-parallel"
       echo "variation=$variation"
-      JVM_OPTS="$gc_opts" $lein run $pars -v $v -t $t -x $variation > $result_path/$variation-v$v-t$t-i$i.txt
+      JVM_OPTS="$gc_opts" $lein run $pars -t $t -x $variation > $result_path/$variation-$pars_filename-t$t-i$i.txt
     done
   done
 done
